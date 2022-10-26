ca453d19a906d3cfd25fc8596e9f3af959ce00c0
bulskov <bulskov@ruc.dk>
2022-10-26 11:12:25 +0200

    setting EF up

---
+++ EF/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EF
+{
+    public class NorthwindContext : DbContext
+    {
+        const string ConnectionString = "host=localhost;db=northwind;uid=bulskov;pwd=henrik";
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseNpgsql(ConnectionString);
+        }
+    }
+}
--- EF/Program.cs
+++ EF/Program.cs
@@ -1,1 +1,13 @@
 ﻿
+
+
+using EF;
+using Microsoft.EntityFrameworkCore.Internal;
+
+using var db = new NorthwindContext();
+
+foreach (var category in db.Categories)
+{
+    Console.WriteLine(category.Name);
+}
+
