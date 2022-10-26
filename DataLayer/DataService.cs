a254459cfa3ee74b5efb697ef0daea92f3440366
bulskov <bulskov@ruc.dk>
2022-10-26 12:01:18 +0200

    dataservice

---
+++ DataLayer/DataService.cs
@@ -0,0 +1,14 @@
+﻿using DataLayer.Model;
+
+namespace DataLayer
+{
+    public class DataService
+    {
+        public IList<Category> GetCategories()
+        {
+            using var db = new NorthwindContext();
+
+            return db.Categories.ToList();
+        }
+    }
+}
---
+++ MayApp/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using DataLayer;
+
+var ds = new DataService();
+
+var categories = ds.GetCategories();
+
+foreach (var category in categories)
+{
+    Console.WriteLine(category.Name);
+}
